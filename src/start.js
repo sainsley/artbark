89c30c7148fea73f2be91021c66ecc0f787e3e82
Krilnon <krilnon@gmail.com>
2013-05-02 23:59:10 +0200

    Add infinite scrolling progress bar to upload. Fixes #4.

--- src/start.js
+++ src/start.js
@@ -7,7 +7,26 @@ function init(){
 	
 	$('#art-file').fileupload({
 		dropZone: $(document),
+		start: addProgressBar,
 		done: function(e, data){ throw 'unhandled successful upload' },
 		fail: function(e, data){ window.location = 'settings.html' }
 	})
+}
+
+function addProgressBar(){
+	var progressBar = $('<div />', {
+		class: 'progress progress-striped active'
+	})
+	
+	var bar = $('<div />', {
+		class: 'bar',
+		css: { width: '100%' }
+	})
+	
+	progressBar.append(bar)
+	$('#upload-area').append(progressBar)
+}
+
+function removeProgressBar(){
+	$('#upload-area').children('.progress').remove()
 }
