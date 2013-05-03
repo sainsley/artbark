3b371039a10a93ac60d68dc7a73529b3ddc5466d
Amy Zhao <xamyzhao@Amys-MacBook-Pro.local>
2013-05-03 01:35:09 -0400

    Propagated user email to art upload page. Will propagate through art upload process tomorrow if it isn't done already.

--- src/start.js
+++ src/start.js
@@ -4,6 +4,30 @@ $(init)
 
 function init(){
 	console.log('start page')
+
+			$.extend({
+				getUrlVars : function() {
+					var vars = [], hash;
+					var hashes = window.location.href.slice(
+							window.location.href.indexOf('?') + 1).split('&');
+					for ( var i = 0; i < hashes.length; i++) {
+						hash = hashes[i].split('=');
+						vars.push(hash[0]);
+						vars[hash[0]] = hash[1];
+					}
+					return vars;
+				},
+				getUrlVar : function(name) {
+					return $.getUrlVars()[name];
+				}
+			});
+
+				if($.getUrlVar('userEmail')) 
+				{
+					var userEmail = $.getUrlVar('userEmail');
+					$("#linkUserEmail")[0].innerHTML = userEmail;
+					//trialsPerUser = (parseInt($.getUrlVar('trialsPerUser')))	
+				}
 	
 	$('#hidden-upload').click($('#art-file').click)
 	$('#cancel-btn').click(onCancel)
